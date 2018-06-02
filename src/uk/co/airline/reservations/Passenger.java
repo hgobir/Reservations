f03f61d8feafe08b609bc89c49b2d714b5dd2fb9
Hamza Gobir <hgobir@googlemail.com>
2018-06-02 08:08:44 +0100

    created initial passenger class

---
+++ src/uk/co/airline/reservations/Passenger.java
@@ -0,0 +1,55 @@
+/*
+ * Passenger.java
+ * author: hgobir
+ */
+
+package uk.co.airline.reservations;
+
+public class Passenger {
+	
+	//fields for passengers on flight
+	private String name;
+	private String address;
+	private int age;
+	
+	//constructor to create passengers
+	public Passenger(String name, String address, int age){
+		
+		this.name = name;
+		this.address = address;
+		this.age = age;	
+	}
+
+	//getters and setters
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+
+}
