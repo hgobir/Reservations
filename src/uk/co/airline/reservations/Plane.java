ff6aa2d95dd3e0ac89ad023cda7995df65ade30c
Hamza Gobir <hgobir@googlemail.com>
2018-06-08 23:29:46 +0100

    small changes to Plane interface

--- src/uk/co/airline/reservations/Plane.java
+++ src/uk/co/airline/reservations/Plane.java
@@ -1,6 +1,15 @@
 package uk.co.airline.reservations;
 
-public interface Plane {
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+public interface Plane {
+	
+	ArrayList<Seat> seatingPlan = new ArrayList<Seat>();
+	boolean inFlight = false;
+	
+	public boolean fly();
+	
+	public LocalDate flightTime();
 
 }
